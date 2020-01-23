5caf90958cceb7bfa734abe137fb5a5c6c5dd1fd
Anthony Smith <as361@students.uwf.edu>
2020-01-23 17:05:49 -0500

    Added unit testing

---
+++ tests/HotelTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelTest {
+
+    @Test
+    void setRooms() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setLocation() {
+    }
+
+    @Test
+    void setNumberOfRooms() {
+    }
+
+    @Test
+    void getRooms() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getLocation() {
+    }
+
+    @Test
+    void getNumberOfRooms() {
+    }
+
+    @Test
+    void addRoom() {
+    }
+
+    @Test
+    void addReservation() {
+    }
+}
